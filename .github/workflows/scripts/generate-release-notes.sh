d0c5f2382cbc9235c4df8d796be380d102829e44
Stanislav Chernyshev <madmasm@gmail.com>
2025-12-27 20:42:04 +0300

    Update generate-release-notes.sh

--- .github/workflows/scripts/generate-release-notes.sh
+++ .github/workflows/scripts/generate-release-notes.sh
@@ -31,7 +31,7 @@ fi
 
 # Create release notes in .genreleases directory
 cat > .genreleases/release-notes.txt << EOF
-This is the latest set of releases that you can use with your agent of choice. We recommend using the Specify CLI to scaffold your projects, however you can download these independently and manage them yourself.
+This is the latest set of releases that you can use with your agent of choice. We recommend using the PLDF CLI to scaffold your projects, however you can download these independently and manage them yourself.
 
 ## Changelog
 
